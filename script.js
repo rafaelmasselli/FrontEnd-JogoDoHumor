d87fb97a058564f01e5604c5c07c4281fbc0800b
rafaelmasselli <rafaelmasselli0@gmail.com>
2021-09-25 02:27:55 -0300

    projeto1

---
+++ script.js
@@ -0,0 +1,33 @@
+const botao = document.querySelector("#humor");
+const enu = document.querySelector("#nucia");
+let imagem = document.querySelector("#imagem1");
+
+botao.addEventListener("click",function(){
+    
+    if (enu.innerHTML == "Morty puto") {
+       imagem.src= "midia/A2.png";
+       enu.innerHTML = "Morty Triste"
+
+
+
+} else if(enu.innerHTML == "Morty Triste"){
+
+        imagem.src = "midia/A3.png"
+        enu.innerHTML = 'Morty besta'
+
+
+
+} else if (enu.innerHTML == "Morty besta"){
+
+        imagem.src= "midia/A4.png"
+        enu.innerHTML ='Morty mafioso'
+
+
+
+ }else if (enu.innerHTML == "Morty mafioso"){
+
+        imagem.src = "midia/A1.png"
+        enu.innerHTML ='Morty puto'
+
+}});
+
