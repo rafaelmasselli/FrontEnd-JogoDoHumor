f5075e36397f8be0abd55c37457738fd127e2a4a
rafaelmasselli <rafaelmasselli0@gmail.com>
2021-09-25 21:44:37 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const botao = document.querySelector("#humor");
 const enu = document.querySelector("#nucia");
 let imagem = document.querySelector("#imagem1");
+var contador = 0;
 
 botao.addEventListener("click",function(){
     
+ if (contador === 10){
+ 
+     imagem.src = "midia/Evil.png"
+     enu.innerHTML = 'Morty Evil'
+
+contador ++  
+
+}
     if (enu.innerHTML == "Morty puto") {
        imagem.src= "midia/A2.png";
        enu.innerHTML = "Morty Triste"
 
+contador++
 
 
 } else if(enu.innerHTML == "Morty Triste"){
@@ -15,6 +25,7 @@ botao.addEventListener("click",function(){
         imagem.src = "midia/A3.png"
         enu.innerHTML = 'Morty besta'
 
+ contador++
 
 
 } else if (enu.innerHTML == "Morty besta"){
@@ -22,6 +33,7 @@ botao.addEventListener("click",function(){
         imagem.src= "midia/A4.png"
         enu.innerHTML ='Morty mafioso'
 
+contador++
 
 
  }else if (enu.innerHTML == "Morty mafioso"){
@@ -29,5 +41,12 @@ botao.addEventListener("click",function(){
         imagem.src = "midia/A1.png"
         enu.innerHTML ='Morty puto'
 
-}});
+contador++
+
+}else if (contador == 11){
+
+        imagem.src = "midia/Evil.png"
+        enu.innerHTML = 'Morty Triste'
+}
+});
 
